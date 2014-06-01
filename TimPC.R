865271468946e649a3216f4e46d535cefe96b3f1
andy-olstad <andy.olstad@bus.oregonstate.edu>
2014-06-01 00:06:27 -0700

    added a graph of years

--- TimPC.R
+++ TimPC.R
@@ -9,7 +9,6 @@ table(data[,1])  # Table of our years...small numbers before 1990's
 years<-table(data[,1])
 plot(1924:2010,years[2:88],type="l",main="Number of songs per year in data set",ylab="Numer of songs")
 
-
 # Splitting the training set and the test set
 X.train <- as.matrix(data[1:463715,2:91])
 X.test <- as.matrix(data[463716:515345, 2:91])
@@ -19,6 +18,20 @@ Y.test <- as.vector(data[463716:515345, 1])
 
 # Tabling the years in the training set
 table(Y.train)
+year<-rep(0,90)
+for(i in 1922:2011){
+year[i-1921]<-length(which(data[,1]==i))
+}
+decade<-rep(0,10)
+for(i in 192:201){
+decade[i-191]<-length(which(trunc(data[,1]/10)==i))
+}
+barplot(decade,names=c("1920s","1930s","1940s",
+"1950s","1960s","1970s","1980s","1990s","2000s","2010-1"),
+ylab="number of songs in training set",main="Highly Uneven Distribution of Songs")
+
+plot(1922:2011,year,type="l",main="Number of Songs per Year in Training Set",ylab="Numer of songs",
+xlab="Year of Song's Release")
 
 # Performing PCA on the Correlation matrix of predictors for the training set
 cor.X <- cor(X.train)
@@ -48,6 +61,6 @@ Res.test <- Y.test-Preds.test
 
 write.csv(Res.test, "Residuals_test.csv")
 plot(Y.test, Res.test, pch=16)
-# Not doing to well...but it is probably coming from the small amount of data before 1990's
+# Not doing too well...but it is probably coming from the small amount of data before 1990's
 
 
