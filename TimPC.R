3dbda84f5ba576d516cde59219fc925ebfe411fb
Sarah Guermond <s.guermond@gmail.com>
2014-06-01 12:08:01 -0700

    Sarah: tried some weird modeling

--- TimPC.R
+++ TimPC.R
@@ -1,3 +1,4 @@
+library(dplyr)
 
 ## Reading in the data (first 10000 rows)
 data <- read.table("YearPredictionMSD.txt", sep=",")
@@ -5,43 +6,11 @@ dim(data)
 unique(data[,1])  # Looking at the years we have
 table(data[,1])  # Table of our years...small numbers before 1990's
 
-#SG: plotting years
-library(ggplot2)
-require(scales)
-year_data <- read.table("YearDistributions.tab", header = F)
-year_dist <- ggplot() + 
-  layer(data = year_data,
-        geom = "line",
-        mapping = aes(x = V2, y = V1)) +
-  labs(x = "Year", y = "Number of songs") +
-  scale_x_continuous(breaks=pretty_breaks(n=10)) +
-  theme_bw(18)
-year_dist
-ggsave("year_dist.pdf")
-
 # Splitting the training set and the test set
 X.train <- as.matrix(data[1:463715,2:91])
 X.test <- as.matrix(data[463716:515345, 2:91])
 Y.train <- as.vector(data[1:463715, 1])
 Y.test <- as.vector(data[463716:515345, 1])
-
-
-# Tabling the years in the training set
-table(Y.train)
-year<-rep(0,90)
-for(i in 1922:2011){
-year[i-1921]<-length(which(data[,1]==i))
-}
-decade<-rep(0,10)
-for(i in 192:201){
-decade[i-191]<-length(which(trunc(data[,1]/10)==i))
-}
-barplot(decade,names=c("1920s","1930s","1940s",
-"1950s","1960s","1970s","1980s","1990s","2000s","2010-1"),
-ylab="number of songs in training set",main="Highly Uneven Distribution of Songs")
-
-plot(1922:2011,year,type="l",main="Number of Songs per Year in Training Set",ylab="Numer of songs",
-xlab="Year of Song's Release")
 
 # Performing PCA on the Correlation matrix of predictors for the training set
 cor.X <- cor(X.train)
@@ -57,7 +26,6 @@ file_out <- cbind(Y.train, new.pred)
 # Writing a CSV file to use: The 8 PCs as new predictors
 write.csv(file_out, "PC_predictors_train.csv")
 
-
 # Running the linear model on the training data
 mod <- lm(Y.train ~ new.pred)
 summary(mod)
@@ -72,6 +40,44 @@ Res.test <- Y.test-Preds.test
 write.csv(Res.test, "Residuals_test.csv")
 plot(Y.test, Res.test, pch=16)
 # Not doing too well...but it is probably coming from the small amount of data before 1990's
+
+# SG: attempting multi-gaussian
+library(mclust)
+new.pred <- read.csv("PC_predictors_train.csv", header = T)
+head(new.pred[,3:9])
+model1 <- Mclust(new.pred[,3:9])
+
+
+# AO: Tabling the years in the training set
+table(Y.train)
+year<-rep(0,90)
+for(i in 1922:2011){
+  year[i-1921]<-length(which(data[,1]==i))
+}
+decade<-rep(0,10)
+for(i in 192:201){
+  decade[i-191]<-length(which(trunc(data[,1]/10)==i))
+}
+barplot(decade,names=c("1920s","1930s","1940s",
+                       "1950s","1960s","1970s","1980s","1990s","2000s","2010-1"),
+        ylab="number of songs in training set",main="Highly Uneven Distribution of Songs")
+
+plot(1922:2011,year,type="l",main="Number of Songs per Year in Training Set",ylab="Numer of songs",
+     xlab="Year of Song's Release")
+
+#SG: plotting years
+library(ggplot2)
+require(scales)
+year_data <- read.table("YearDistributions.tab", header = F)
+year_dist <- ggplot() + 
+  layer(data = year_data,
+        geom = "line",
+        mapping = aes(x = V2, y = V1)) +
+  labs(x = "Year", y = "Number of songs") +
+  scale_x_continuous(breaks=pretty_breaks(n=10)) +
+  theme_bw(18)
+year_dist
+ggsave("year_dist.pdf")
 
 # SG: plot predictions
 library(dplyr)
